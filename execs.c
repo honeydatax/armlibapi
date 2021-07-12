e30c84aa4d781a71a2f61557999de01093681a70
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-07-12 01:12:23 +0100

    Gcc -c -o execs.o execs.c

--- execs.c
+++ execs.c
@@ -1,5 +1,16 @@
+#include <stdio.h>
 #include <sys/mman.h>
 
-int execss(void *adds,int lens){
-	mprotect(adds,(size_t)lens,PROT_READ | PROT_WRITE | PROT_EXEC);
+int *execss(int lens){
+	int *ptr=mmap(NULL,lens,PROT_READ | PROT_WRITE | PROT_EXEC,MAP_PRIVATE | MAP_ANONYMOUS ,0,0);
+	if(ptr==MAP_FAILED){
+		ptr=(int *)-1;
+	}
+	return ptr;
 }
+int uexecss(int *ptr,int lens){
+	int ptrs=munmap(ptr,lens);
+	return ptrs;
+}
+
+
